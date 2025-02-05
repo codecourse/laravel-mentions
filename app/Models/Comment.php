4725983e8ed5a5ba1f3553b5ca1683db87a5a715
Alex Garrett-Smith <alex@codecourse.com>
2025-02-05 11:52:30 +0000

    Episode 07

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use App\Observers\CommentObserver;
+use Fico7489\Laravel\Pivot\Traits\PivotEventTrait;
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -12,8 +13,18 @@ class Comment extends Model
 {
     /** @use HasFactory<\Database\Factories\CommentFactory> */
     use HasFactory;
+    use PivotEventTrait;
 
     protected $guarded = false;
+
+    public static function booted()
+    {
+        static::pivotAttached(function ($model, $relationName, $pivotIds) {
+            if ($relationName === 'mentions') {
+                // notify the user(s)
+            }
+        });
+    }
 
     public function user()
     {
